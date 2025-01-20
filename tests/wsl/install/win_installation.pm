c7c5470d1cb38cc28286866b3985abbf89d597b6
Pablo Herranz Ramírez <pherranz@suse.com>
2025-01-20 15:00:50 +0100

    Remove all attended installation references

---
+++ tests/wsl/install/win_installation.pm
@@ -0,0 +1,81 @@
+# SUSE's openQA tests
+#
+# Copyright 2016-2018 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Windows 10 installation test module
+#    modiffied (only win10 drivers) iso from https://fedoraproject.org/wiki/Windows_Virtio_Drivers is needed
+#    Works only with CDMODEL=ide-cd and QEMUCPU=host or core2duo (maybe other but not qemu64)
+# Maintainer: Jozef Pupava <jpupava@suse.com>
+
+use base "windowsbasetest";
+use strict;
+use warnings;
+
+use testapi;
+
+sub prepare_win11 {
+    # Change some regedit values to skip system requirements check
+    send_key 'shift-f10';
+    assert_screen 'windows-install-cmd';
+    type_string "reg add HKEY_LOCAL_MACHINE\\SYSTEM\\Setup /v LabConfig\n";
+    type_string "reg add HKEY_LOCAL_MACHINE\\SYSTEM\\Setup\\LabConfig /v BypassTPMCheck /t REG_DWORD /d 1\n";
+    type_string "reg add HKEY_LOCAL_MACHINE\\SYSTEM\\Setup\\LabConfig /v BypassRAMCheck /t REG_DWORD /d 1\n";
+    type_string "reg add HKEY_LOCAL_MACHINE\\SYSTEM\\Setup\\LabConfig /v BypassSecureBootCheck /t REG_DWORD /d 1\n";
+    type_string "exit\n";
+}
+
+sub run {
+
+    my $self = shift;
+
+    if (get_var('UEFI')) {
+        while (check_screen('windows-boot', timeout => 5)) {
+            send_key 'spc';
+            record_info('SPC', 'Space key pressed');
+        }    # boot from CD or DVD
+    }
+
+    $self->wait_boot_windows;
+    record_info('Windows firstboot', 'Starting Windows for the first time');
+    wait_still_screen stilltime => 60, timeout => 300;
+    # When starting Windows for the first time, several screens or pop-ups may appear
+    # in a different order. We'll try to handle them until the desktop is shown
+    assert_screen(['windows-desktop', 'windows-edge-decline', 'networks-popup-be-discoverable', 'windows-start-menu', 'windows-qemu-drivers'], timeout => 120);
+    while (not match_has_tag('windows-desktop')) {
+        assert_and_click 'network-discover-yes' if (match_has_tag 'networks-popup-be-discoverable');
+        assert_and_click 'windows-edge-decline' if (match_has_tag 'windows-edge-decline');
+        assert_and_click 'windows-start-menu' if (match_has_tag 'windows-start-menu');
+        assert_and_click 'windows-qemu-drivers' if (match_has_tag 'windows-qemu-drivers');
+        wait_still_screen stilltime => 15, timeout => 60;
+        assert_screen(['windows-desktop', 'windows-edge-decline', 'networks-popup-be-discoverable', 'windows-start-menu', 'windows-qemu-drivers'], timeout => 120);
+    }
+
+    # TODO: all this part should be added to the unattend XML in the future
+
+    # Setup stable lock screen background
+    record_info('Config lockscreen', 'Setup stable lock screen background');
+    $self->use_search_feature('lock screen settings');
+    assert_screen 'windows-lock-screen-in-search';
+    wait_still_screen stilltime => 2, timeout => 10, similarity_level => 43;
+    assert_and_click 'windows-lock-screen-in-search', dclick => 1;
+    assert_screen 'windows-lock-screen-settings';
+    assert_and_click 'windows-lock-screen-background';
+    assert_and_click 'windows-select-picture';
+    assert_and_click 'windows-close-lockscreen';
+    wait_still_screen stilltime => 2, timeout => 10, similarity_level => 43;
+
+    # These commands disable notifications that Windows shows randomly and
+    # make our windows lose focus
+    $self->open_powershell_as_admin;
+    $self->run_in_powershell(cmd => 'reg add "HKLM\Software\Policies\Microsoft\Windows" /v Explorer');
+    $self->run_in_powershell(cmd => 'reg add "HKLM\Software\Policies\Microsoft\Windows\Explorer" /v DisableNotificationCenter /t REG_DWORD /d 1');
+    $self->run_in_powershell(cmd => 'reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\PushNotifications" /v ToastEnabled /t REG_DWORD /d 0');
+    # Prevent password from expiring:
+    $self->run_in_powershell(cmd => 'wmic UserAccount set PasswordExpires=False');
+    record_info 'Port close', 'Closing serial port...';
+    $self->run_in_powershell(cmd => '$port.close()', code => sub { });
+    $self->run_in_powershell(cmd => 'exit', code => sub { });
+}
+
+1;
