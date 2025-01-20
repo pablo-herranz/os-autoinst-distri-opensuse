9b6c525149ac3eeea85d536871008bca347728b1
Pablo Herranz Ramírez <pherranz@suse.com>
2025-01-20 15:33:56 +0100

    Move commands to the XML file

--- tests/wsl/install/win_installation.pm
+++ tests/wsl/install/win_installation.pm
@@ -50,32 +50,6 @@ sub run {
         wait_still_screen stilltime => 15, timeout => 60;
         assert_screen(['windows-desktop', 'windows-edge-decline', 'networks-popup-be-discoverable', 'windows-start-menu', 'windows-qemu-drivers'], timeout => 120);
     }
-
-    # TODO: all this part should be added to the unattend XML in the future
-
-    # Setup stable lock screen background
-    # record_info('Config lockscreen', 'Setup stable lock screen background');
-    # $self->use_search_feature('lock screen settings');
-    # assert_screen 'windows-lock-screen-in-search';
-    # wait_still_screen stilltime => 2, timeout => 10, similarity_level => 43;
-    # assert_and_click 'windows-lock-screen-in-search', dclick => 1;
-    # assert_screen 'windows-lock-screen-settings';
-    # assert_and_click 'windows-lock-screen-background';
-    # assert_and_click 'windows-select-picture';
-    # assert_and_click 'windows-close-lockscreen';
-    # wait_still_screen stilltime => 2, timeout => 10, similarity_level => 43;
-
-    # These commands disable notifications that Windows shows randomly and
-    # make our windows lose focus
-    $self->open_powershell_as_admin;
-    $self->run_in_powershell(cmd => 'reg add "HKLM\Software\Policies\Microsoft\Windows" /v Explorer');
-    $self->run_in_powershell(cmd => 'reg add "HKLM\Software\Policies\Microsoft\Windows\Explorer" /v DisableNotificationCenter /t REG_DWORD /d 1');
-    $self->run_in_powershell(cmd => 'reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\PushNotifications" /v ToastEnabled /t REG_DWORD /d 0');
-    # Prevent password from expiring:
-    $self->run_in_powershell(cmd => 'wmic UserAccount set PasswordExpires=False');
-    record_info 'Port close', 'Closing serial port...';
-    $self->run_in_powershell(cmd => '$port.close()', code => sub { });
-    $self->run_in_powershell(cmd => 'exit', code => sub { });
 }
 
 1;
